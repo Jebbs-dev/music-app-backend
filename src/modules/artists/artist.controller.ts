78d745d3d2369f5286654ce55506592f091bd2c5
Jebbs-dev <ojebiyifulness@gmail.com>
2025-07-05 23:17:40 +0100

    feat: add artist controller with crud endpoints

---
+++ src/modules/artists/artist.controller.ts
@@ -0,0 +1,47 @@
+import { Controller, Get, Post, Body, Patch, Param } from '@nestjs/common';
+import { ArtistService } from './artist.service';
+import { Artist } from 'generated/prisma';
+
+@Controller('artists')
+export class ArtistController {
+  constructor(private readonly artistService: ArtistService) {}
+
+  @Post()
+  async createArtist(@Body() artistData: Artist) {
+    return this.artistService.createArtist(artistData);
+  }
+
+  @Patch(':artistId')
+  async updateArtist(
+    @Param('artistId') artistId: string,
+    @Body() artistData: Partial<Artist>,
+  ) {
+    return this.artistService.updateArtist(artistId, artistData);
+  }
+
+  @Get()
+  async fetchAllArtists(
+    @Param()
+    filters: {
+      search?: string;
+      skip?: string;
+      take?: string;
+      sortBy?: string;
+      sortOrder?: 'asc' | 'desc';
+      startDate?: string;
+      endDate?: string;
+    },
+  ) {
+    return this.artistService.fetchAllArtists(filters);
+  }
+
+  @Get(':artistId')
+  async fetchArtistById(@Param('artistId') artistId: string) {
+    return this.artistService.fetchArtistById(artistId);
+  }
+
+  @Get('name/:artistName')
+  async fetchArtistByName(@Param('artistName') artistName: string) {
+    return this.artistService.fetchArtistByName(artistName);
+  }
+}
