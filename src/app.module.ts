946125ac737ec12ba18dc802baf03caf3c1b9f3f
Jebbs-dev <ojebiyifulness@gmail.com>
2025-07-06 22:58:59 +0100

    feat: update app module imports
    Updates AppModule to import UserModule, ArtistModule, SongModule, and PrismaModule, and marks the module as global.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { UserModule } from './modules/users/user.module';
+import { ArtistModule } from './modules/artists/artist.module';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
+import { SongModule } from './modules/songs/song.module';
 
+@Global()
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    PrismaModule,
+    UserModule,
+    ArtistModule,
+    SongModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
