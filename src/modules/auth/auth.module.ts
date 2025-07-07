ae98e676bcc6ab1f7bfd34912c77211e2f1cd79d
Jebbs-dev <ojebiyifulness@gmail.com>
2025-07-07 23:37:30 +0100

    build: add auth module with jwt and dependencies
    Introduces AuthModule, configuring JWT authenticationProvides AuthService and AuthController, and exports AuthService and JwtModule for use in other modules.

---
+++ src/modules/auth/auth.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { UserModule } from '../users/user.module';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthController } from './auth.controller';
+import { jwtConstants } from './constants/constants';
+import { ArtistModule } from '../artists/artist.module';
+
+@Module({
+  imports: [
+    UserModule,
+    ArtistModule,
+    JwtModule.register({
+      global: true,
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
+  providers: [AuthService],
+  controllers: [AuthController],
+})
+export class AuthModule {}
