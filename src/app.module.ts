61f7f024c68be5341d43154dd114bb8797f5e46e
Jebbs-dev <ojebiyifulness@gmail.com>
2025-07-07 23:38:27 +0100

    add auth module to app module imports
    This is to enable authentication features across the application.

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,11 +6,13 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 import { SongModule } from './modules/songs/song.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Global()
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    AuthModule,
     PrismaModule,
     UserModule,
     ArtistModule,
