0b025fe5d9246de50eae89ad410c44ed9eb663ab
Jebbs-dev <ojebiyifulness@gmail.com>
2025-07-10 23:57:31 +0100

    feat: add global authentication guard
    This adds a global guard to protected routes with Public decorator to mark unprotected routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,13 @@ import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 import { SongModule } from './modules/songs/song.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from './modules/auth/auth.guard';
+import { SetMetadata } from '@nestjs/common';
+import { AlbumModule } from './modules/album/album.module';
+
+export const IS_PUBLIC_KEY = 'isPublic';
+export const Public = () => SetMetadata(IS_PUBLIC_KEY, true);
 
 @Global()
 @Module({
@@ -16,9 +23,16 @@ import { AuthModule } from './modules/auth/auth.module';
     PrismaModule,
     UserModule,
     ArtistModule,
+    AlbumModule,
     SongModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
 })
 export class AppModule {}
