7004c071f70666857873cf72193aaa7373258e5c
Jebbs-dev <ojebiyifulness@gmail.com>
2025-07-11 22:18:09 +0100

    update login parameters with dto

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,13 +1,16 @@
 import { Body, Controller, Post, HttpCode, HttpStatus } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { Public } from '../../common/decorators/public.decorator';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @HttpCode(HttpStatus.OK)
+  @Public()
   @Post('login')
-  signIn(@Body() email: string, password: string) {
-    return this.authService.signIn(email, password);
+  signIn(@Body() params: LoginDto) {
+    return this.authService.signIn(params);
   }
 }
