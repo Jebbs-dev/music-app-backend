b0c32d42208ee6cf43b1ae1bc7529c82f922d8c7
Jebbs-dev <ojebiyifulness@gmail.com>
2025-07-29 22:47:31 +0100

    feat: add constants for access and refresh tokens

--- src/modules/auth/constants/constants.ts
+++ src/modules/auth/constants/constants.ts
@@ -1,3 +1,6 @@
 export const jwtConstants = {
-  secret: process.env.JWT_SECRET,
+  accessTokenSecret: process.env.ACCESS_TOKEN_SECRET || '',
+  refreshTokenSecret: process.env.REFRESH_TOKEN_SECRET || '',
+  accessTokenExpiresIn: '15m', // Short-lived access token
+  refreshTokenExpiresIn: '7d', // Longer-lived refresh token
 };
