d1d3cd8bfa2ef1401029dbdca9d99f20700352f2
Jebbs-dev <ojebiyifulness@gmail.com>
2025-07-30 22:51:50 +0100

    feat: add token refresh endpoint

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Post, HttpCode, HttpStatus } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { Public } from '../../common/decorators/public.decorator';
 import { LoginDto } from './dto/login.dto';
+import RefreshTokenDto from './dto/refresh-token.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -12,5 +13,11 @@ export class AuthController {
   @Post('login')
   signIn(@Body() params: LoginDto) {
     return this.authService.signIn(params);
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Post('refresh')
+  refreshToken(@Body() refreshTokenDto: RefreshTokenDto) {
+    return this.authService.refreshToken(refreshTokenDto);
   }
 }
