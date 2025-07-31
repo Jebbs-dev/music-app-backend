c8cf844183d0fd29f95f38bf2530fa81f13ac716
Jebbs-dev <ojebiyifulness@gmail.com>
2025-07-31 21:22:53 +0100

    update access token duration

--- src/modules/auth/constants/constants.ts
+++ src/modules/auth/constants/constants.ts
@@ -1,6 +1,6 @@
 export const jwtConstants = {
   accessTokenSecret: process.env.ACCESS_TOKEN_SECRET || '',
   refreshTokenSecret: process.env.REFRESH_TOKEN_SECRET || '',
-  accessTokenExpiresIn: '15m', // Short-lived access token
+  accessTokenExpiresIn: '1h', // Short-lived access token
   refreshTokenExpiresIn: '7d', // Longer-lived refresh token
 };
