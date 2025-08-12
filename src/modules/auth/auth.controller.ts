ec4660590737283b46b94a855677b371669ad75a
Jebbs-dev <ojebiyifulness@gmail.com>
2025-08-12 15:53:01 +0100

    update auth controller

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,8 +1,17 @@
-import { Body, Controller, Post, HttpCode, HttpStatus } from '@nestjs/common';
-import { AuthService } from './auth.service';
+import {
+  Body,
+  Controller,
+  Post,
+  HttpCode,
+  HttpStatus,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthService, AuthResult } from './auth.service';
 import { Public } from '../../common/decorators/public.decorator';
 import { LoginDto } from './dto/login.dto';
 import RefreshTokenDto from './dto/refresh-token.dto';
+import { AuthGuard } from './auth.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -11,7 +20,7 @@ export class AuthController {
   @HttpCode(HttpStatus.OK)
   @Public()
   @Post('login')
-  signIn(@Body() params: LoginDto) {
+  signIn(@Body() params: LoginDto): Promise<AuthResult> {
     return this.authService.signIn(params);
   }
 
@@ -19,5 +28,15 @@ export class AuthController {
   @Post('refresh')
   refreshToken(@Body() refreshTokenDto: RefreshTokenDto) {
     return this.authService.refreshToken(refreshTokenDto);
+  }
+
+  @Post('logout-all-devices')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @UseGuards(AuthGuard)
+  async logoutAllDevices(
+    @Request() req: { user: { userId: string; type: 'USER' | 'ARTIST' } },
+  ) {
+    const { userId, type } = req.user;
+    await this.authService.logoutAllDevices(userId, type);
   }
 }
